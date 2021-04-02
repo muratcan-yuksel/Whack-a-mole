874c1487ca8607cc43d433147bb8734282154844
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-02 06:24:12 +0300

    added an array of objects called squares

---
+++ app.js
@@ -0,0 +1,18 @@
+const squares = [
+  {
+    name: 'first',
+    img: './images/blank.png',
+  },
+  {
+    name: 'second',
+    img: './images/blank.png',
+  },
+  {
+    name: 'third',
+    img: './images/blank.png',
+  },
+  {
+    name: 'fourth',
+    img: './images/blank.png',
+  },
+];
