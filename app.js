315c60cca8fde3f4c6d207c22c7bb75a483e4781
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-02 07:46:05 +0300

    changed button ids

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-// const squares = [
-//   {
-//     name: 'first',
-//     img: './images/white.png',
-//   },
-//   {
-//     name: 'second',
-//     img: './images/white.png',
-//   },
-//   {
-//     name: 'third',
-//     img: './images/white.png',
-//   },
-//   {
-//     name: 'fourth',
-//     img: './images/white.png',
-//   },
-// ];
-
+// get the squares and style them
 const squares = document.querySelectorAll('.squares');
 squares.forEach((item) => {
   item.style.width = '50px';
   item.style.height = '50px';
   item.style.border = '1px solid black';
-  item.style.background = 'blue';
+  item.style.background = 'grey';
   item.style.borderRadius = '50%';
+});
+// get the squares
+const square1 = document.getElementById('square1');
+const square2 = document.getElementById('square2');
+const square3 = document.getElementById('square3');
+const square4 = document.getElementById('square4');
+
+const button = document.getElementById('button1');
+button.addEventListener('click', function () {
+  one.style.background = 'black';
 });
