08f6012f1f373c305a8f273c336be9703c4847fe
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-03 04:23:26 +0300

    just coyping, btw, deleted the eslintrc bcs it wouldn't let me assing a let variable

--- app.js
+++ app.js
@@ -1,67 +1,17 @@
-// get the squares and style them
-const squares = document.querySelectorAll('.squares');
-squares.forEach((item) => {
-  item.style.width = '50px';
-  item.style.height = '50px';
-  item.style.border = '1px solid black';
-  item.style.background = 'grey';
-  item.style.borderRadius = '50%';
-});
-// get the squares
-const square1 = document.getElementById('square1');
-const square2 = document.getElementById('square2');
-const square3 = document.getElementById('square3');
-const square4 = document.getElementById('square4');
-// get buttons
-const button2 = document.getElementById('button-2');
-const button11 = document.getElementById('button-1-1');
-const button12 = document.getElementById('button-1-2');
-
-// button2.addEventListener('click', function () {
-//   square2.style.background = 'black';
-// });
-
-function btn2() {
-  if (square1.style.background == 'grey') {
-    square1.style.background = 'black';
-    square2.style.background = 'black';
-  } else if (
-    square1.style.background == 'black' &&
-    square3.style.background == 'grey'
-  ) {
-    square2.style.background = 'black';
-    square3.style.background = 'black';
-  } else if (
-    square1.style.background == 'black' &&
-    square3.style.background == 'black'
-  ) {
-    square2.style.background = 'black';
-    square4.style.background = 'black';
-    alert('good job!');
+const grid = document.querySelector('.grid');
+// create grid items
+function createGrid() {
+  for (let i = 1; i < 10; i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.setAttribute('class', 'square');
+    gridDiv.setAttribute('id', i);
+    grid.appendChild(gridDiv);
   }
-  // disable after 1 click
-  button2.disabled = true;
 }
 
-button2.addEventListener('click', btn2);
+createGrid();
 
-function btn11() {
-  if (square1.style.background == 'grey') {
-    square1.style.background = 'black';
-  } else if (square1.style.background == 'black') {
-    alert('miss');
-  }
-  button11.disabled = true;
-}
-
-button11.addEventListener('click', btn11);
-
-function btn12() {
-  if (square3.style.background == 'grey') {
-    square3.style.background = 'black';
-  } else if (square3.style.background == 'black') {
-    alert('miss');
-  }
-  button12.disabled = true;
-}
-button12.addEventListener('click', btn12);
+const square = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.getElementById('time-left');
+let score = document.getElementById('score');
