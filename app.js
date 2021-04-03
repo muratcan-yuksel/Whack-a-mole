33ef4b58666f2e4f92d7a9a1ed71a45337affe21
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-04-03 04:52:33 +0300

    nah, it was shit

--- app.js
+++ app.js
@@ -15,3 +15,48 @@ const square = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.getElementById('time-left');
 let score = document.getElementById('score');
+
+ let result= 0;
+ let currentTime = timeLeft.textContent;
+//okay this is beneficial and smart
+function randomSquare(){
+    //starts by removing all the classes if there's one
+    square.forEach(className=>{
+        className.classList.remove("mole")
+    })
+    //and randomly adds the mole class so that each time this function gets invoked,
+    //a random square will get the mole class,
+    //which has an image on the css
+    let randomPosition=square[Math.floor(Math.random() *9)]
+    randomPosition.classList.add("mole");
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+      if(id.id === hitPosition){
+        result = result + 1
+        score.textContent = result
+        hitPosition=null
+      }
+    })
+  })
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+  }
+  
+  moveMole()
+  
+  
+  function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+  
+    if(currentTime === 0 ) {
+      clearInterval(timerId)
+      alert('GAME OVER! Your final score is' + result)
+    }
+  }
+  
+  let timerId = setInterval(countDown, 1000)
